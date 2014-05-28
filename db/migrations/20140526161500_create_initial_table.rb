74b6c83c812895774e2503eab81907f061ef0e58
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-28 13:43:57 +0200

    [TODO] Remove an answered TODO.

--- db/migrations/20140526161500_create_initial_table.rb
+++ db/migrations/20140526161500_create_initial_table.rb
@@ -1,18 +1,14 @@
-# TODO Will this be a problem if we have multiple apps accessing the same migrations table?
-# (What happens if one of the migrations in the table is not found in the project, because
-# it came from another project?)
-#
 Sequel.migration do
   change do
     create_table(:metrics) do
       primary_key :id
       foreign_key :pod_id, :pods, :null => false, :key => [:id]
 
-      Integer  :github_stars, :null => true
-      Integer  :github_forks, :null => true
-      Integer  :github_watchers, :null => true
-      Integer  :github_contributors, :null => true
-      Integer  :github_pull_requests, :null => true
+      Integer :github_stars, :null => true
+      Integer :github_forks, :null => true
+      Integer :github_watchers, :null => true
+      Integer :github_contributors, :null => true
+      Integer :github_pull_requests, :null => true
       DateTime :github_first_commit, :null => true
       DateTime :github_last_commit, :null => true
 
