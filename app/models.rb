a17f87864ff93a36a312a12b8175818b747561a7
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-07 23:33:19 +0200

    [Models] Load the new models.

--- app/models.rb
+++ app/models.rb
@@ -1,2 +1,6 @@
 require File.expand_path '../models/github_metrics', __FILE__
+require File.expand_path '../models/commit', __FILE__
+require File.expand_path '../models/version', __FILE__
 require File.expand_path '../models/pod', __FILE__
+
+Sequel::Model.plugin :update_or_create
