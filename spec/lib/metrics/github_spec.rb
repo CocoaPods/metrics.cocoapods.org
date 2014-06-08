55041a979d928c878081b8f96ac0a051769f4298
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-08 19:00:34 +0200

    [Github Metrics] Add handling of non-standard and erroneous repo URLs.

--- spec/lib/metrics/github_spec.rb
+++ spec/lib/metrics/github_spec.rb
@@ -4,22 +4,52 @@ require File.expand_path('../../../../lib/metrics/github', __FILE__)
 
 describe Metrics::Github do
 
-  describe 'with a github URL' do
+  describe 'with a .git github URL' do
     before do
-      @github = Metrics::Github.new 'https://github.com/lakesoft/LKAssetsLibrary.git'
+      @github = Metrics::Github.new 'https://github.com/myusername/reponame.git'
     end
     it 'has the right user' do
-      @github.user.should == 'lakesoft'
+      @github.user.should == 'myusername'
     end
     it 'has the right repo' do
-      @github.repo.should == 'LKAssetsLibrary'
+      @github.repo.should == 'reponame'
     end
-    # it 'updates the metrics model correctly' do
-    #   pod = Pod.create(:name => 'MetricsTestPod')
-    #   metrics = GithubMetrics.create(:pod => pod)
-    #   @github.update metrics
-    #   metrics.should == ''
-    # end
+  end
+
+  describe 'with a non .git github URL' do
+    before do
+      @github = Metrics::Github.new 'https://github.com/myusername/reponame'
+    end
+    it 'has the right user' do
+      @github.user.should == 'myusername'
+    end
+    it 'has the right repo' do
+      @github.repo.should == 'reponame'
+    end
+  end
+
+  describe 'with a private github URL' do
+    before do
+      @github = Metrics::Github.new 'git@github.com/myusername/reponame.git'
+    end
+    it 'has the right user' do
+      @github.user.should == 'myusername'
+    end
+    it 'has the right repo' do
+      @github.repo.should == 'reponame'
+    end
+  end
+
+  describe 'with a ? github URL' do
+    before do
+      @github = Metrics::Github.new 'git://github.com/myusername/reponame.git'
+    end
+    it 'has the right user' do
+      @github.user.should == 'myusername'
+    end
+    it 'has the right repo' do
+      @github.repo.should == 'reponame'
+    end
   end
 
 end
